a5e0368fc8d553cbb9efefd401675641419505fc
Stephen Perreira <stephenperreira@gmail.com>
2019-01-28 19:10:42 -0800

    Misc

---
+++ client/src/views/ToDos.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Hi from todos</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class Todos extends Vue {
+    
+}
+
+</script>
