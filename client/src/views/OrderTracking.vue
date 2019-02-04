8c654d864d90a10d71fb631ae5675c660e410c74
gfilangeri <gfilange@calpoly.edu>
2019-02-03 17:30:49 -0800

    added to order tracking page

--- client/src/views/OrderTracking.vue
+++ client/src/views/OrderTracking.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="main-background">Order Tracking</div>
+  <div class="main-background track">
+    <p class="title">Track Your Order</p>
+    <input class="input-bar" placeholder="Order Number" type="text">
+    <button class="button submit-button">Track</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,3 +15,31 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class OrderTracking extends Vue {
 }
 </script>
+
+<style lang="scss">
+
+  .track {
+    padding:100px 20px 30px 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .title {
+    font-size: 50px;
+    text-decoration: none;
+  }
+
+  .input-bar {
+    padding:5px;
+    font-size: 25px;
+    width: 50%;
+    height: 50px;
+  }
+
+  .submit-button {
+    margin: 20px;
+  }
+
+</style>
