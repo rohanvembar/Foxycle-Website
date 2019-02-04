2efb8b75bd4efcd4c85aa89205f966ca08d4b032
gfilangeri <gfilange@calpoly.edu>
2019-02-03 17:00:11 -0800

    added to services

--- client/src/views/Services.vue
+++ client/src/views/Services.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="main-background cards">
-    <div>
-      <font-awesome-icon icon="sync-alt" />
-      Tune-Ups
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="sync-alt" />
+        <p class="card-title-text">Tune-Ups</p>
+      </div>
+      <div class="card-body">
+        <p class="section-title">Basic Tune-Up - $25</p>
+        Includes brake inspection and adjustments, derailleur inspection and adjustments, tire inspection and inflation, safety check of all bolts and wipe down of frame.
+
+        <p class="section-title">Premium Tune-Up - $35</p>
+        Everything included in the Basic Tune-Up, plus wheel truing, pivot point lube, chain clean and lube and frame clean and polish.
+
+        <p class="section-title">Brake System Tune-Up - $45</p>
+        Includes brake pad installation, brake inspection and adjustments and inspection of cables and housing.
+
+      </div>
     </div>
-    <div class="border-middles">
-      <font-awesome-icon icon="wrench" />
-      Adjustments & Repairs
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="wrench" />
+        <p class="card-title-text">Adjustments & Repairs</p>
+      </div>
+      <div class="card-body">
+        body
+      </div>
     </div>
-    <div>
-      <font-awesome-icon icon="arrow-alt-circle-down" />
-      Installations
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="arrow-alt-circle-down" />
+        <p class="card-title-text">Installations</p>
+      </div>
+      <div class="card-body">
+        body
+      </div>
     </div>
   </div>
 </template>
@@ -39,11 +62,34 @@ export default class Services extends Vue {
 .cards {
   padding:30px;
   display: flex;
-  align-items: center;
+  align-items: top;
   justify-content: space-around;
 }
-.border-middles {
-  border-left: 1px black;
-  border-right: 1px black;
+
+.indivisual-card {
+  background-color: white;
+  width: 27%;
+  border-radius: 30px;
+}
+
+.card-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top:30px;
+}
+
+.card-body {
+  padding: 20px;
+}
+
+.section-title {
+  padding-top:10px;
+  font-weight: bold;
+  font-size: 15px;
+}
+
+.card-title-text {
+  padding-left: 5px;
 }
 </style>
