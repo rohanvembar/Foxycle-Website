9e17afa501f8fd6014f306aa40c120bf50621ab1
Stephen Perreira <stephenperreira@gmail.com>
2019-02-04 15:59:55 -0800

    Added H-Tags

--- client/src/views/ItemPage.vue
+++ client/src/views/ItemPage.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="main-background">Item Page</div>
+  <div class="main-background">
+    <div class="main-content"> 
+      <div class="title">
+        <h2>Foxcycle Extreme Pro 500</h2>
+        <h5>Built For Something</h5>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
