3422800c30164e0f30f9532e62d2e6c370c54698
gfilangeri <gfilange@calpoly.edu>
2019-02-03 22:49:36 -0800

    added table to cart and link to checkout

--- client/src/views/Cart.vue
+++ client/src/views/Cart.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="main-background">Cart</div>
+  <div class="main-background cart">
+    <div class="cart-table">
+      <table>
+        <tr>
+          <th>Product</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total</th>
+        </tr>
+        <tr>
+          <td> <img src="../assets/transparentlogo.png"></td>
+          <td>bike</td>
+          <td>bike</td>
+          <td>bike</td>
+        </tr>
+        <tr>
+          <td> <img src="../assets/transparentlogo.png"></td>
+          <td>bike</td>
+          <td>bike</td>
+          <td>bike</td>          
+        </tr>
+        <tr>
+          <td> <img src="../assets/transparentlogo.png"></td>
+          <td>bike</td>
+          <td>bike</td>
+          <td>bike</td>          
+        </tr>      
+        <tr>
+          <td>Subtotal</td>
+          <td></td>
+          <td></td>
+          <td>$50000</td>          
+        </tr>    
+        <tr>
+          <td>Shipping</td>
+          <td></td>
+          <td></td>
+          <td>$100</td>          
+        </tr> 
+        <tr>
+          <td>Total</td>
+          <td></td>
+          <td></td>
+          <td>$50100</td>          
+        </tr>                         
+      </table>
+    </div>
+    <div class="checkout-btn">
+      <router-link class="button" to="/checkout" exact-active-class="is-active">
+        Checkout
+      </router-link></div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,3 +62,26 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class Cart extends Vue {
 }
 </script>
+
+<style scoped>
+
+.cart {
+  padding: 30px 300px 30px 300px;
+}
+.cart-table {
+  display: flex;
+  align-items: center;
+  justify-content: center;     
+}
+
+th, td{
+  padding: 10px 30px 10px 30px;
+}
+
+.checkout-btn {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;     
+}
+</style>
+
