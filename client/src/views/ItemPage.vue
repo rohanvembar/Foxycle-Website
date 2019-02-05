a9a2c5e619eaaf54dc6264c4473ab2d9f1a94993
Stephen Perreira <stephenperreira@gmail.com>
2019-02-04 16:25:18 -0800

    Added custom scss for item page

--- client/src/views/ItemPage.vue
+++ client/src/views/ItemPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-background">
-    <div class="main-content"> 
-      <div class="title">
+    <div class="itempage-main-content"> 
+      <div class="itempage-title">
         <h2>Foxcycle Extreme Pro 500</h2>
         <h5>Built For Something</h5>
       </div>
@@ -14,7 +14,12 @@ import axios, { AxiosResponse } from "axios";
 import { APIConfig } from "../utils/api.utils";
 import { Component, Prop, Vue } from "vue-property-decorator";
 
+
 @Component
 export default class ItemPage extends Vue {
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../assets/itempage.scss";
+</style>
