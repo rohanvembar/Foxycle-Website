1c7fdaffb0519945138cec12cdba0b50a140f085
Rohan Vembar <rohanvembar@gmail.com>
2019-02-04 18:53:38 -0800

    update todo rohan branch

---
+++ api/controller/todo.controller.ts
@@ -0,0 +1,31 @@
+import DefaultController from "./default.controller";
+import express from "express";
+import { NextFunction, Request, Response, Router } from "expresS";
+import { getRepository } from "typeorm";
+import { Session, ToDo } from "../entity";
+
+export class TodoController extends DefaultController {
+
+    protected initializeRoutes(): express.Router {
+        const router = express.Router();
+
+        router.route("/todos").post((req: Request, res: Response) => {
+            const token = req.get("token");
+            const sessionRepo = getRepository(Session);
+            const todoRepo = getRepository(ToDo);
+            const todo = new ToDo();
+            sessionRepo.findOne(token).then((foundSession: Session | undefined) => {
+                const user = foundSession!.user;
+                todo.dueDate = req.body.dueDate;
+                todo.title = req.body.title;
+                todo.user = user;
+                todoRepo.save(todo).then((savedTodo : ToDo) => {
+                    res.status(200).send({todo});
+                });
+            });
+            
+        });
+
+        return router;
+    }
+}
---
+++ api/entity/todo.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm";
+import { User } from "../entity";
+
+@Entity()
+export class ToDo {
+
+    @PrimaryGeneratedColumn()
+    public id!: number;
+
+    @Column()
+    public title!: string;
+
+    @Column()
+    public complete!: boolean;
+
+    @Column()
+    public dueDate!: Date;
+
+    @OneToOne((type) => User, {cascade: true})
+    @JoinColumn()
+    public user!: User;
+}
