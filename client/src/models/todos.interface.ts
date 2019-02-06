ad75524872a416ef5e2dc26d7342225ea4ae18e1
Rohan Vembar <rohanvembar@gmail.com>
2019-02-05 19:56:33 -0800

    merge

---
+++ client/src/models/todos.interface.ts
@@ -0,0 +1,7 @@
+export interface iTodos {
+    user: string,
+    dueDate: Date,
+    complete: string,
+    title: string,
+    id: number
+  }
--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -6,19 +6,19 @@
                 <th>Item</th>
                 <th>Due Date</th>
             </tr>
-            <tr v-for="(todo, index) in mytodos" :key="index">
+            <!-- <tr v-for="(todo, index) in mytodos" :key="index">
                 <td>{{ todo.name }}</td>
                 <td>{{ todo.duedate }}</td>
-            </tr>
+            </tr> -->
         </table>
         </center>
         <br>
         <center>
         <form>
-            Item Name: <input type="text" placeholder="What do you want to add?" v-model="name"/>
+            Item Name: <input type="text" placeholder="What do you want to add?" v-model="todo.title"/>
             <br>
-            Due Date: <input type="date" v-model="duedate"/>
-            <button type="button" v-on:click="addTodoItem()">Add</button>
+            Due Date: <input type="date" v-model="todo.dueDate"/>
+            <button type="button" v-on:click="pushToDB">Add</button>
         </form>
         </center>
     </div> 
@@ -27,23 +27,54 @@
 <script lang='ts'>
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator';
+import axios, { AxiosResponse } from "axios";
+import { APIConfig } from "../utils/api.utils";
+import { iTodos } from "../models/todos.interface";
+
 @Component
 export default class ToDos extends Vue {
     mytodos: Todo[] = [
 
     ];
 
-    addTodoItem(){
-        console.log("hello" + this.mytodos.values());
-        this.mytodos.push({name: this.name, duedate: this.duedate})
+    todo: Todo = {
+        user: "",
+        dueDate: new Date(0),
+        complete: "",
+        title: ""
+    };
+    error: string | boolean = false;
+
+
+    pushToDB(){
+        //console.log("hello" + this.mytodos.values());
+        //this.mytodos.push({name: this.name, duedate: this.duedate})
+        debugger;
+        this.error = false;
+        // this.signup.firstName = "done";
+        console.log('hello');
+        axios
+        .post(APIConfig.buildUrl("/todos"), {
+            ...this.todo
+        })
+        .then((response: AxiosResponse<iTodos>) => {
+            this.$emit("success");
+        })
+        .catch((errorResponse: any) => {
+            debugger;
+            this.error = errorResponse.response.data.reason;
+        });
     }
 
     
 } 
 
 
-interface Todo {
-    name: string;
-    duedate: Date;
+export interface Todo {
+    user: string,
+    dueDate: Date,
+    complete: string,
+    title: string,
+
 }
 </script>
