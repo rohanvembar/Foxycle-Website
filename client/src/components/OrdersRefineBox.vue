b7f980fbe237e8e76a5e00ce563cefa356f55312
sangmin6810 <35266080+sangmin6810@users.noreply.github.com>
2019-02-11 10:41:41 -0800

    checkbox

--- client/src/components/OrdersRefineBox.vue
+++ client/src/components/OrdersRefineBox.vue
@@ -1,16 +1,31 @@
 <template>
     <div class="refine-box">
         <p class="refine-title">Refine By</p>
-        <p>Order Status</p>
-        <select name="status">
-            <option selected="true" value="all">All</option>
-            <option value="0">Not Started</option>
-            <option value="1">In Progress</option>
-            <option value="2">Complete</option>  
-            <option value="3">Canceled</option>     
-        </select>         
+        <div>
+            <label class="container">Not Started
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <div>
+            <label class="container">In Progress
+                <input type="checkbox">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <div>
+            <label class="container">Complete
+                <input type="checkbox">
+                <span class="checkmark"></span>
+            </label>
+        </div>
+        <div>
+            <label class="container">Canceled
+                <input type="checkbox">
+                <span class="checkmark"></span>
+            </label>    
+        </div> 
     </div>
- 
 </template>
 
 <script lang="ts">
@@ -23,7 +38,7 @@ export default class OrdersRefineBox extends Vue {
 
 <style scoped lang="scss">
  .refine-box {
-    height : 270px;
+    height : 200px;
     background : white;
     text-align : left;
     margin-bottom: 40px;
@@ -31,5 +46,71 @@ export default class OrdersRefineBox extends Vue {
 }
 .refine-title{
     font-weight: bold;
+}
+.container {
+    position: relative;
+    padding-left: 35px;
+    padding-bottom: 35px;
+    margin-bottom: 32px;
+    cursor: pointer;
+    font-size: 22px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
 }
 </style>
