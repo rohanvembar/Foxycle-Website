83f56e5a6c986cc14989bc2a4dabcff47e512649
Rohan Vembar <rohanvembar@gmail.com>
2019-02-20 13:58:33 -0800

    add buttons in edit info

--- client/src/views/AdminEditInfo.vue
+++ client/src/views/AdminEditInfo.vue
@@ -19,15 +19,31 @@
 
     <div v-else>
       <div class="main-background column-sections">
-        <div class="row-sections">
-          <button v-on:click="changePageAnnouncements">Manage Announcements</button>
-          <button v-on:click="changePageServices">Manage Services</button>
-          <button v-on:click="changePageContact">Manage Contact</button>
+        <div class="row-sections buttons">
+          <button class="button is-rounded is-focused is-info" v-on:click="changePageAnnouncements">
+            <span class="icon is-medium">
+              <i class="fas fa-bullhorn"></i>
+            </span>
+            <span>edit announcements</span>
+          </button>
+          <button
+            class="button is-rounded is-focused is-info"
+            v-on:click="changePageServices"
+          ><span class="icon is-medium">
+              <i class="fas fa-concierge-bell"></i>
+            </span>
+            <span>edit services</span></button>
+          <button
+            class="button is-rounded is-focused is-info"
+            v-on:click="changePageContact"
+          ><span class="icon is-medium">
+              <i class="fas fa-info-circle"></i>
+            </span>
+            <span>edit contact info</span></button>
         </div>
         <EditAnnouncements v-if="announcements"/>
         <EditServices v-if="services"/>
         <EditContact v-if="contact"/>
-
       </div>
     </div>
   </div>
@@ -55,8 +71,8 @@ export default class AdminEditInfo extends Vue {
   }
   error: string | boolean = false;
   announcements: boolean = true;
-  services: boolean = false
-  contact: boolean = false
+  services: boolean = false;
+  contact: boolean = false;
 
   changePageAnnouncements() {
     this.announcements = true;
@@ -66,12 +82,12 @@ export default class AdminEditInfo extends Vue {
   changePageServices() {
     this.announcements = false;
     this.services = true;
-    this.contact = false;    
+    this.contact = false;
   }
   changePageContact() {
     this.announcements = false;
     this.services = false;
-    this.contact = true;    
+    this.contact = true;
   }
 }
 </script>
@@ -89,6 +105,7 @@ export default class AdminEditInfo extends Vue {
   display: flex;
   justify-content: space-evenly;
   align-items: flex-start;
+  padding: 15px;
 }
 .column-sections {
   display: flex;
