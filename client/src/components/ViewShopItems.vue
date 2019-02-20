f1633a81fb3c632aa646f5d6172e116ce98f785a
Rohan Vembar <rohanvembar@gmail.com>
2019-02-20 12:12:51 -0800

    add banner if on sale

--- client/src/components/ViewShopItems.vue
+++ client/src/components/ViewShopItems.vue
@@ -9,9 +9,9 @@
     <ShopPageFilterBox v-if="hasItems()"/>
 
     <div id="wrap" v-if="hasItems()">
-
       <div id="columns" class="columns_4">
-        <figure v-for="(item, index) in sortedItems" v-bind:key="index">
+        <figure class="has-ribbon" v-for="(item, index) in sortedItems" v-bind:key="index">
+          <div class="ribbon is-danger" v-if="item.price <= 100">{{ saleText }}</div>
           <router-link :to="{ name: 'itempage', params: { itemid: item.id } }">
             <div class="imagediv">
               <img :src="item.image" class="image">
@@ -54,10 +54,10 @@ import ShopPageFilterBox from "@/components/ShopPageFilterBox.vue";
   }
 })
 export default class ViewShopItems extends Vue {
-  @Prop() sortVal: String
+  @Prop() sortVal: String;
   error: string | boolean = false;
   items: iShopItem[] = [];
-
+  saleText: string = "50% Off"
   toast(item: iShopItem) {
     const ele = document.getElementById("toast");
     if (ele) {
@@ -98,24 +98,19 @@ export default class ViewShopItems extends Vue {
       });
   }
 
-  get sortedItems(){
-    function compareName(a, b){
-      if(a.name < b.name)
-        return -1;
-      if((a.name > b.name))
-        return 1;
+  get sortedItems() {
+    function compareName(a, b) {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
       return 0;
     }
-    function comparePrice(a, b){
-      if(a.price < b.price)
-        return -1;
-      if((a.price > b.price))
-        return 1;
+    function comparePrice(a, b) {
+      if (a.price < b.price) return -1;
+      if (a.price > b.price) return 1;
       return 0;
     }
 
-    if(this.sortVal == "1")
-      return this.items.sort(compareName);
+    if (this.sortVal == "1") return this.items.sort(compareName);
     return this.items.sort(comparePrice);
   }
 
