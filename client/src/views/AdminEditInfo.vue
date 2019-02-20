5f6b57c8a7da185423b47e869fef07b58477a79b
gfilangeri <gfilange@calpoly.edu>
2019-02-20 01:03:45 -0800

    started edit services

--- client/src/views/AdminEditInfo.vue
+++ client/src/views/AdminEditInfo.vue
@@ -19,8 +19,87 @@
 
     <div v-else>
       <div class="main-background row-sections">
+        <div class="column-sections">
+          <div class="message is-info">
+            <h1 class="message-header">New Announcement</h1>
+            <div class="announcement-input">
+              <input
+                class="input-field"
+                type="text"
+                v-model="newAnnouncementTitle"
+                placeholder="title"
+              >
+              <textarea
+                class="input-field"
+                type="text"
+                v-model="newAnnouncementBody"
+                placeholder="body"
+              ></textarea>
+              <button class="button input-field" v-on:click="newAnnouncement()">Save</button>
+            </div>
+          </div>
+          <div class="message is-info">
+            <h1 class="message-header">Manage Announcements</h1>
+            <div v-for="(a, index) in announcements" v-bind:key="index" class="message is-info">
+              <div class="message-header">
+                <p>{{a.title}}</p>
+                <p>{{a.date}}</p>
+              </div>
+              <p class="message-body">{{a.body}}</p>
+              <div class="delete-button">
+                <button v-on:click="deleteAnnouncement(a)" class="button">delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="message is-info">
-          <h1 class="message-header">New Announcement</h1>
+          <h1 class="message-header">Manage Services</h1>
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="sync-alt"/>
+        <p class="card-title-text">Tune-Ups</p>
+      </div>
+      <div class="card-body">
+        <div class="servicesTuneUps">
+          <article v-for="(a, index) in servicesTuneUps" v-bind:key="index">
+            <p class="section-title">{{a.title}} - ${{a.price}}</p>
+            <p>{{a.description}}</p>
+          </article>
+        </div>
+      </div>
+    </div>
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="wrench"/>
+        <p class="card-title-text">Adjustments & Repairs</p>
+      </div>
+      <div class="card-body">
+        <div class="servicesAdjust">
+          <article v-for="(a, index) in servicesAdjust" v-bind:key="index">
+            <p class="section-title">{{a.title}} - ${{a.price}}</p>
+            <p>{{a.description}}</p>
+          </article>
+        </div>
+      </div>
+    </div>
+    <div class="indivisual-card">
+      <div class="card-title">
+        <font-awesome-icon icon="arrow-alt-circle-down"/>
+        <p class="card-title-text">Installations</p>
+      </div>
+      <div class="card-body">
+        <div class="servicesInst">
+          <article v-for="(a, index) in servicesInst" v-bind:key="index">
+            <p class="section-title">{{a.title}} - ${{a.price}}</p>
+            <p>{{a.description}}</p>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+        </div>
+        <div class="message is-info">
+          <h1 class="message-header">Manage Contact</h1>
           <div class="announcement-input">
             <input
               class="input-field"
@@ -37,17 +116,6 @@
             <button class="button input-field" v-on:click="newAnnouncement()">Save</button>
           </div>
         </div>
-        <div class="message is-info">
-          <h1 class="message-header">Manage Announcements</h1>
-          <div v-for="(a, index) in announcements" v-bind:key="index" class="message is-info">
-            <div class="message-header">
-              <p>{{a.title}}</p>
-              <p>{{a.date}}</p>
-            </div>
-            <p class="message-body">{{a.body}}</p>
-            <div class="delete-button "><button v-on:click="deleteAnnouncement(a)" class="button">delete</button></div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
@@ -59,6 +127,7 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 import { APIConfig } from "../utils/api.utils";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { iAnnouncement } from "../models/announcement.interface";
+import { iService } from '@/models/service.interface';
 
 @Component({})
 export default class AdminEditInfo extends Vue {
@@ -68,10 +137,14 @@ export default class AdminEditInfo extends Vue {
   error: string | boolean = false;
   newAnnouncementTitle: String = "";
   newAnnouncementBody: String = "";
+  servicesTuneUps: iService[] = [];
+  servicesAdjust: iService[] = [];
+  servicesInst: iService[] = [];
   announcements: iAnnouncement[] = [];
 
   created() {
     this.getAllAnnouncements();
+    this.getAllServices();
   }
 
   getAllAnnouncements() {
@@ -92,7 +165,58 @@ export default class AdminEditInfo extends Vue {
       });
   }
 
-  deleteAnnouncement(announcement : iAnnouncement) {
+  getAllServices() {
+    this.error = false;
+    axios
+      .get(APIConfig.buildUrl("/servicesTuneUps"), {
+        headers: {
+          token: this.$store.state.userToken
+        }
+      })
+      .then((response: AxiosResponse) => {
+        this.servicesTuneUps = response.data;
+        console.log(response.data);
+        this.$emit("success");
+      })
+      .catch((res: AxiosError) => {
+        this.error = res.response && res.response.data.error;
+        console.log(this.error);
+      });
+
+    axios
+      .get(APIConfig.buildUrl("/servicesAdjust"), {
+        headers: {
+          token: this.$store.state.userToken
+        }
+      })
+      .then((response: AxiosResponse) => {
+        this.servicesAdjust = response.data;
+        console.log(response.data);
+        this.$emit("success");
+      })
+      .catch((res: AxiosError) => {
+        this.error = res.response && res.response.data.error;
+        console.log(this.error);
+      });
+
+    axios
+      .get(APIConfig.buildUrl("/servicesInst"), {
+        headers: {
+          token: this.$store.state.userToken
+        }
+      })
+      .then((response: AxiosResponse) => {
+        this.servicesInst = response.data;
+        console.log(response.data);
+        this.$emit("success");
+      })
+      .catch((res: AxiosError) => {
+        this.error = res.response && res.response.data.error;
+        console.log(this.error);
+      });
+  }
+
+  deleteAnnouncement(announcement: iAnnouncement) {
     this.error = false;
     axios
       .delete(APIConfig.buildUrl("/announcements/" + announcement.id))
@@ -120,7 +244,7 @@ export default class AdminEditInfo extends Vue {
       })
       .then((response: AxiosResponse) => {
         console.log("[AdminEditInfo.vue]" + JSON.stringify(response.data));
-        this.announcements.unshift(response.data)
+        this.announcements.unshift(response.data);
         this.$emit("success");
         this.newAnnouncementTitle = "";
         this.newAnnouncementBody = "";
@@ -183,6 +307,6 @@ export default class AdminEditInfo extends Vue {
 .delete-button {
   display: flex;
   justify-content: flex-end;
-  align-items: center;    
+  align-items: center;
 }
 </style>
