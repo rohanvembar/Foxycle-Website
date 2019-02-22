8084ad779507c44ab2c0d4e5b3ded23e8391bb66
Rohan Vembar <rohanvembar@gmail.com>
2019-02-22 00:33:43 -0800

    added fancy credit card to checkout and made it look slightly better

--- client/src/views/Checkout.vue
+++ client/src/views/Checkout.vue
@@ -2,150 +2,166 @@
   <div class="main">
     <div class="columns is-centered">
       <div class="column">
-        <label class="label">shipping address</label>
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="first name">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </p>
-            </div>
+        <div class="title">Shipping Address</div>
+        <div class="space">
+          <div class="field inputbox">
+            <label>First Name</label>
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="first name">
+              <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+              </span>
+            </p>
           </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="last name">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </p>
-            </div>
+          <div class="field inputbox">
+            <label>Last Name</label>
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="last name">
+              <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+              </span>
+            </p>
           </div>
-        </div>
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="address">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
+          <div class="field inputbox">
+            <label>Email</label>
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="email">
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+            </p>
           </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="city">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
+          <div class="field inputbox">
+            <label>Street Address</label>
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="street address">
+              <span class="icon is-small is-left">
+                <i class="fas fa-map-marker-alt"></i>
+              </span>
+            </p>
           </div>
-        </div>
-
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="state">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
+          <div class="field inputbox">
+            <label>City</label>
+            <label class="right">State</label>
+            <p class="control is-expanded">
+              <input class="input city" type="text" placeholder="city">
+              <span class="select state">
+                <select name="state">
+                  <option value="AL">AL</option>
+                  <option value="AK">AK</option>
+                  <option value="AR">AR</option>
+                  <option value="AZ">AZ</option>
+                  <option value="CA">CA</option>
+                  <option value="CO">CO</option>
+                  <option value="CT">CT</option>
+                  <option value="DC">DC</option>
+                  <option value="DE">DE</option>
+                  <option value="FL">FL</option>
+                  <option value="GA">GA</option>
+                  <option value="HI">HI</option>
+                  <option value="IA">IA</option>
+                  <option value="ID">ID</option>
+                  <option value="IL">IL</option>
+                  <option value="IN">IN</option>
+                  <option value="KS">KS</option>
+                  <option value="KY">KY</option>
+                  <option value="LA">LA</option>
+                  <option value="MA">MA</option>
+                  <option value="MD">MD</option>
+                  <option value="ME">ME</option>
+                  <option value="MI">MI</option>
+                  <option value="MN">MN</option>
+                  <option value="MO">MO</option>
+                  <option value="MS">MS</option>
+                  <option value="MT">MT</option>
+                  <option value="NC">NC</option>
+                  <option value="NE">NE</option>
+                  <option value="NH">NH</option>
+                  <option value="NJ">NJ</option>
+                  <option value="NM">NM</option>
+                  <option value="NV">NV</option>
+                  <option value="NY">NY</option>
+                  <option value="ND">ND</option>
+                  <option value="OH">OH</option>
+                  <option value="OK">OK</option>
+                  <option value="OR">OR</option>
+                  <option value="PA">PA</option>
+                  <option value="RI">RI</option>
+                  <option value="SC">SC</option>
+                  <option value="SD">SD</option>
+                  <option value="TN">TN</option>
+                  <option value="TX">TX</option>
+                  <option value="UT">UT</option>
+                  <option value="VT">VT</option>
+                  <option value="VA">VA</option>
+                  <option value="WA">WA</option>
+                  <option value="WI">WI</option>
+                  <option value="WV">WV</option>
+                  <option value="WY">WYYYYY</option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="zip code">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
+          <div class="field inputbox">
+            <label>Zip Code</label>
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="zip code">
+              <span class="icon is-small is-left">
+                <i class="fas fa-globe"></i>
+              </span>
+            </p>
           </div>
         </div>
       </div>
-      <div class="column">
-        <label class="label">billing address</label>
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input id="ship-first-name" class="input" type="text" placeholder="first name">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input id="ship-last-name" class="input" type="text" placeholder="last name">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-user"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="address">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="city">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
 
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="state">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-          <div class="field-body">
-            <div class="field">
-              <p class="control is-expanded has-icons-left">
-                <input class="input" type="text" placeholder="zip code">
-                <span class="icon is-small is-left">
-                  <i class="fas fa-globe"></i>
-                </span>
-              </p>
-            </div>
-          </div>
+      <div class="column">
+        <div class="title">Billing Info</div>
+
+        <div class="card-wrapper">
+          <card v-model="cardDetail"></card>
+        </div>
+        <div class="centercard">
+          <input
+            class="input inputbox"
+            name="number"
+            placeholder="Card number"
+            type="tel"
+            v-model="cardDetail.number"
+            v-card-focus
+          >
+          <input
+            class="input inputbox"
+            name="name"
+            placeholder="Full name"
+            type="text"
+            v-model="cardDetail.name"
+            v-card-focus
+          >
+          <p class="control is-expanded centercard">
+            <input
+              class="input inputbox city"
+              name="expiry"
+              placeholder="MM/YY"
+              type="tel"
+              v-model="cardDetail.expiry"
+              v-card-focus
+            >
+            <input
+              class="input inputbox state"
+              name="cvc"
+              placeholder="CVC"
+              type="number"
+              v-model="cardDetail.cvc"
+              v-card-focus
+            >
+          </p>
         </div>
       </div>
       <div class="column">
         <div class="center">
           <div>
+            <div class="title">Order Summary</div>
             <table class="center">
-              <h3>Order Summary</h3>
               <tr>
                 <th>Product</th>
                 <th></th>
@@ -182,19 +198,18 @@
               <tr>
                 <td></td>
                 <td></td>
-                <td>
-                  <div class="checkout-btn">
-                    <div class="button is-success is-rounded" v-on:click="loading(), placeOrder()">
-                      <i class="fas fa-check iconpadding"></i>place order
-                    </div>
-
-                    <div id="pageloader" class="pageloader is-info">
-                      <span class="title">just a sec, your order is being submitted</span>
-                    </div>
-                  </div>
-                </td>
+                <td></td>
               </tr>
             </table>
+            <div class="checkout-btn">
+              <div class="button is-success is-rounded" v-on:click="loading(), placeOrder()">
+                <i class="fas fa-check iconpadding"></i>place order
+              </div>
+
+              <div id="pageloader" class="pageloader is-info">
+                <span class="title">just a sec, your order is being submitted</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -212,8 +227,20 @@ import { APIConfig } from "../utils/api.utils";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { iShopItem } from "../models/shopitem.interface";
 import { iCart } from "../models/cart.interface";
+import card from "vue-credit-card";
 
-@Component
+let defaultProps = {
+  number: "5555555555554444",
+  name: "",
+  expiry: "",
+  cvc: ""
+};
+
+@Component({
+  components: {
+    card
+  }
+})
 export default class Checkout extends Vue {
   items: iShopItem[] = this.$store.state.items;
   cart: iCart[] = [];
@@ -221,15 +248,20 @@ export default class Checkout extends Vue {
   shipping: number = 10;
   total: number = 0;
   ordernumber: string = "";
-  
-
+  name: "Card";
+  components: {
+    card;
+  };
+  data() {
+    return {
+      cardDetail: defaultProps
+    };
+  }
   computeSubtotal() {
     for (var i in this.items) {
-      
       if (this.items[i].saleprice) {
         this.subtotal += this.items[i].saleprice;
-      }
-      else {
+      } else {
         this.subtotal += this.items[i].price;
       }
     }
@@ -247,12 +279,15 @@ export default class Checkout extends Vue {
       flag = true;
       for (var j in this.cart) {
         if (this.cart[j].item.id === this.items[i].id) {
-          this.cart[j] = {item: this.cart[j].item, quantity: this.cart[j].quantity + 1}
+          this.cart[j] = {
+            item: this.cart[j].item,
+            quantity: this.cart[j].quantity + 1
+          };
           flag = false;
-        } 
+        }
       }
       if (flag) {
-        this.cart.push({item: this.items[i], quantity: 1})
+        this.cart.push({ item: this.items[i], quantity: 1 });
       }
     }
   }
@@ -297,14 +332,21 @@ export default class Checkout extends Vue {
     var date = new Date();
     var mm = date.getMonth() + 1;
     var dd = date.getDate();
-    var prettyDate = (mm > 9 ? "" : "0") + mm + "." + (dd > 9 ? "" : "0") + dd + "." + date.getFullYear();
-    console.log("[Checkout.vue] date ordered: " + prettyDate)
+    var prettyDate =
+      (mm > 9 ? "" : "0") +
+      mm +
+      "." +
+      (dd > 9 ? "" : "0") +
+      dd +
+      "." +
+      date.getFullYear();
+    console.log("[Checkout.vue] date ordered: " + prettyDate);
     return prettyDate;
   }
 
   placeOrder() {
     this.ordernumber = this.generate();
-    var orderdate = this.parseDate()
+    var orderdate = this.parseDate();
     axios
       .post(APIConfig.buildUrl("/neworder"), {
         orderNumber: this.ordernumber,
@@ -325,16 +367,19 @@ export default class Checkout extends Vue {
 
 <style lang="scss" scoped>
 .main {
-  padding-top: 15px;
+  padding-top: 30px;
   margin-left: auto;
   margin-right: auto;
 
-  width: 90%;
+  width: 80%;
 }
-
+.centercard {
+  text-align: center;
+}
 .center {
   margin-left: auto;
   margin-right: auto;
+  text-align: center;
 }
 
 .iconpadding {
@@ -392,6 +437,12 @@ th {
   border-bottom: black 2px solid;
 }
 
+.title {
+  font-size: 25px;
+  padding-bottom: 15px;
+  border-bottom: 2px solid #239cec;
+  text-align: center;
+}
 .bot-bord {
   border-top: black 2px solid;
 }
@@ -403,5 +454,28 @@ th {
 .iName {
   padding-right: 35px;
 }
+
+.inputbox {
+  width: 300px;
+}
+
+.city {
+  display: inline-block;
+  width: 200px;
+}
+.state {
+  display: inline-block;
+  width: 100px;
+}
+
+.right {
+  padding-left: 175px;
+}
+
+.card-wrapper {
+  padding-bottom: 50px;
+  padding-top: 25px;
+}
+
 </style>
 
